2dd536e62b5ae243d16190072b4e50f28f7e880d
Joe <7lucky7sleven7@gmail.com>
2025-06-22 17:31:42 -0500

    Update FishGame.tsx

--- src/FishGame.tsx
+++ src/FishGame.tsx
@@ -27,17 +27,48 @@ const FishGame: React.FC = () => {
 
     const game = new Phaser.Game(config);
 
+    const fishTypes = [
+      { key: 'fish1', value: 10, speed: 100 },
+      { key: 'fish2', value: 20, speed: 150 },
+      { key: 'fish3', value: 50, speed: 80 },
+    ];
+
     let fish: Phaser.Physics.Arcade.Image;
-    let bulletGroup: Phaser.Physics.Arcade.Group;
     let shootSound: Phaser.Sound.BaseSound;
     let hitSound: Phaser.Sound.BaseSound;
 
     function preload(this: Phaser.Scene) {
       this.load.image('bg', '/fishgame/bg.png');
-      this.load.image('fish', '/fishgame/fish.png');
       this.load.image('bullet', '/fishgame/bullet.png');
       this.load.audio('shoot', '/sounds/shoot.wav');
       this.load.audio('hit', '/sounds/hit.wav');
+
+      fishTypes.forEach(f => this.load.image(f.key, `/fishgame/${f.key}.png`));
+    }
+
+    function spawnFish(this: Phaser.Scene) {
+      const chosen = Phaser.Math.RND.pick(fishTypes);
+
+      fish = this.physics.add.image(
+        Phaser.Math.Between(50, 750),
+        Phaser.Math.Between(50, 500),
+        chosen.key
+      );
+
+      fish.setData('value', chosen.value);
+      fish.setVelocity(chosen.speed, chosen.speed / 2);
+      fish.setCollideWorldBounds(true);
+      fish.setBounce(1);
+
+      // 🐟 Wiggle animation using tween
+      this.tweens.add({
+        targets: fish,
+        angle: { from: -10, to: 10 },
+        duration: 300,
+        yoyo: true,
+        repeat: -1,
+        ease: 'Sine.easeInOut',
+      });
     }
 
     function create(this: Phaser.Scene) {
@@ -46,33 +77,21 @@ const FishGame: React.FC = () => {
       shootSound = this.sound.add('shoot');
       hitSound = this.sound.add('hit');
 
-      fish = this.physics.add.image(100, 100, 'fish');
-      fish.setVelocity(100, 40);
-      fish.setCollideWorldBounds(true);
-      fish.setBounce(1);
-
-      bulletGroup = this.physics.add.group();
+      const bullets = this.physics.add.group();
+      spawnFish.call(this);
 
       this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
         const bullet = this.physics.add.image(400, 560, 'bullet');
         this.physics.moveTo(bullet, pointer.x, pointer.y, 400);
-        bulletGroup.add(bullet);
+        bullets.add(bullet);
         shootSound.play();
 
         this.physics.add.overlap(bullet, fish, () => {
           bullet.destroy();
-          fish.destroy();
           hitSound.play();
-          setScore(prev => prev + 10);
-
-          fish = this.physics.add.image(
-            Phaser.Math.Between(50, 750),
-            Phaser.Math.Between(50, 500),
-            'fish'
-          );
-          fish.setVelocity(100, 40);
-          fish.setCollideWorldBounds(true);
-          fish.setBounce(1);
+          setScore(prev => prev + (fish.getData('value') || 0));
+          fish.destroy();
+          spawnFish.call(this);
         });
       });
     }
