f87911f779b3758e20a209112874c6b728c0590f
Joe <7lucky7sleven7@gmail.com>
2025-06-22 18:46:29 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-
 import FishGame from './FishGame';
+import PurchasePage from './PurchasePage';
 
 export default function App() {
+  const path = window.location.pathname;
+  if (path === '/purchase') return <PurchasePage />;
   return <FishGame />;
 }
