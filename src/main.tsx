a5dbb2d84a09ae04e3a2cec39acd5307f1fed260
Joe <7lucky7sleven7@gmail.com>
2025-06-24 07:07:42 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div style={{ color: 'white', background: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <h1>Hello from React!</h1>
-    </div>
-  </React.StrictMode>
+  <h1 style={{ color: 'white', background: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    ✅ React loaded!
+  </h1>
 );
